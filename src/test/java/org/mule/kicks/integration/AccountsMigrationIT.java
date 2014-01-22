fbc1a7aa8efd2b336878b179c96f365b3071b36d
Cesar Augusto Garcia <cesar.garcia@mulesoft.com>
2014-01-22 19:51:23 -0300

    Commit adding some changes that previous commit

--- src/test/java/org/mule/kicks/integration/AccountsMigrationIT.java
+++ src/test/java/org/mule/kicks/integration/AccountsMigrationIT.java
@@ -1,6 +1,8 @@
 package org.mule.kicks.integration;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -14,30 +16,198 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mule.MessageExchangePattern;
 import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
 import org.mule.api.config.MuleProperties;
+import org.mule.api.context.notification.ServerNotification;
+import org.mule.construct.Flow;
 import org.mule.processor.chain.SubflowInterceptingChainLifecycleWrapper;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.probe.PollingProber;
+import org.mule.tck.probe.Probe;
+import org.mule.tck.probe.Prober;
+import org.mule.transport.NullPayload;
+
+import com.mulesoft.module.batch.api.BatchJobInstance;
+import com.mulesoft.module.batch.api.notification.BatchNotification;
+import com.mulesoft.module.batch.api.notification.BatchNotificationListener;
+import com.mulesoft.module.batch.engine.BatchJobInstanceAdapter;
+import com.mulesoft.module.batch.engine.BatchJobInstanceStore;
+import com.sforce.soap.partner.SaveResult;
 
 /**
- * The objective of this class is to validate the correct behavior of the flows
- * for this Mule Kick that make calls to external systems.
+ * The objective of this class is to validate the correct behavior of the Mule
+ * Kick that make calls to external systems.
  * 
+ * The test will invoke the batch process and afterwards check that the accounts
+ * had been correctly created and that the ones that should be filtered are not
+ * in the destination sand box.
+ * 
+ * @author cesar.garcia
  */
 public class AccountsMigrationIT extends AbstractKickTestCase {
 
-	@Before
-	public void setUp() {
+	protected static final int TIMEOUT = 60;
 
+	private Prober prober;
+	protected Boolean failed;
+	protected BatchJobInstanceStore jobInstanceStore;
+
+	private static SubflowInterceptingChainLifecycleWrapper checkAccountflow;
+	private static List<Map<String, String>> createdAccounts = new ArrayList<Map<String, String>>();
+
+	protected class BatchWaitListener implements BatchNotificationListener {
+
+		public synchronized void onNotification(ServerNotification notification) {
+			final int action = notification.getAction();
+
+			if (action == BatchNotification.JOB_SUCCESSFUL || action == BatchNotification.JOB_STOPPED) {
+				failed = false;
+			} else if (action == BatchNotification.JOB_PROCESS_RECORDS_FAILED || action == BatchNotification.LOAD_PHASE_FAILED || action == BatchNotification.INPUT_PHASE_FAILED
+					|| action == BatchNotification.ON_COMPLETE_FAILED) {
+
+				failed = true;
+			}
+		}
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		failed = null;
+		jobInstanceStore = muleContext.getRegistry().lookupObject(BatchJobInstanceStore.class);
+		muleContext.registerListener(new BatchWaitListener());
+
+		checkAccountflow = getSubFlow("retrieveAccountFlow");
+		checkAccountflow.initialise();
+
+		createTestDataInSandBox();
 	}
 
 	@After
-	public void tearDown() {
+	public void tearDown() throws Exception {
+		failed = null;
+		deleteTestDataFromSandBox();
 	}
 
 	@Test
 	public void testMainFlow() throws Exception {
-           //TODO: Add here a valid tests 
-	    Assert.assertTrue("You should really be adding integration tests to you kick.", false);
+		Flow flow = getFlow("mainFlow");
+		MuleEvent event = flow.process(getTestEvent("", MessageExchangePattern.REQUEST_RESPONSE));
+		BatchJobInstance batchJobInstance = (BatchJobInstance) event.getMessage().getPayload();
+
+		this.awaitJobTermination();
+
+		Assert.assertTrue(this.wasJobSuccessful());
+
+		batchJobInstance = this.getUpdatedInstance(batchJobInstance);
+
+		Assert.assertEquals("The account should not have been sync", null, invokeRetrieveAccountFlow(checkAccountflow, createdAccounts.get(0)));
+
+		Map<String, String> payload = invokeRetrieveAccountFlow(checkAccountflow, createdAccounts.get(1));
+		Assert.assertEquals("The account should have been sync", createdAccounts.get(1).get("Email"), payload.get("Email"));
 	}
 
+	@SuppressWarnings("unchecked")
+	private Map<String, String> invokeRetrieveAccountFlow(SubflowInterceptingChainLifecycleWrapper flow, Map<String, String> account) throws Exception {
+		Map<String, String> accountMap = new HashMap<String, String>();
+
+		accountMap.put("Name", account.get("Name"));
+
+		MuleEvent event = flow.process(getTestEvent(accountMap, MessageExchangePattern.REQUEST_RESPONSE));
+		Object payload = event.getMessage().getPayload();
+		if (payload instanceof NullPayload) {
+			return null;
+		} else {
+			return (Map<String, String>) payload;
+		}
+	}
+
+	protected void awaitJobTermination() throws Exception {
+		this.awaitJobTermination(TIMEOUT);
+	}
+
+	protected void awaitJobTermination(long timeoutSecs) throws Exception {
+		this.prober = new PollingProber(timeoutSecs * 1000, 500);
+		this.prober.check(new Probe() {
+
+			@Override
+			public boolean isSatisfied() {
+				return failed != null;
+			}
+
+			@Override
+			public String describeFailure() {
+				return "batch job timed out";
+			}
+		});
+	}
+
+	protected boolean wasJobSuccessful() {
+		return this.failed != null ? !this.failed : false;
+	}
+
+	protected BatchJobInstanceAdapter getUpdatedInstance(BatchJobInstance jobInstance) {
+		return this.jobInstanceStore.getJobInstance(jobInstance.getOwnerJobName(), jobInstance.getId());
+	}
+
+	@SuppressWarnings("unchecked")
+	private void createTestDataInSandBox() throws MuleException, Exception {
+		SubflowInterceptingChainLifecycleWrapper flow = getSubFlow("createAccountFlow");
+		flow.initialise();
+
+		// This account should not be sync
+		Map<String, String> account = createAccount("A", 0);
+		account.put("MailingCountry", "ARG");
+		createdAccounts.add(account);
+
+		// This account should BE sync
+		account = createAccount("A", 1);
+		createdAccounts.add(account);
+
+		MuleEvent event = flow.process(getTestEvent(createdAccounts, MessageExchangePattern.REQUEST_RESPONSE));
+		List<SaveResult> results = (List<SaveResult>) event.getMessage().getPayload();
+		for (int i = 0; i < results.size(); i++) {
+			createdAccounts.get(i).put("Id", results.get(i).getId());
+		}
+	}
+
+	private void deleteTestDataFromSandBox() throws MuleException, Exception {
+		// Delete the created accounts in A
+		SubflowInterceptingChainLifecycleWrapper flow = getSubFlow("deleteAccountFromAFlow");
+		flow.initialise();
+
+		List<String> idList = new ArrayList<String>();
+		for (Map<String, String> c : createdAccounts) {
+			idList.add(c.get("Id"));
+		}
+		flow.process(getTestEvent(idList, MessageExchangePattern.REQUEST_RESPONSE));
+
+		// Delete the created accounts in B
+		flow = getSubFlow("deleteAccountFromBFlow");
+		flow.initialise();
+		idList.clear();
+		for (Map<String, String> c : createdAccounts) {
+			Map<String, String> account = invokeRetrieveAccountFlow(checkAccountflow, c);
+			if (account != null) {
+				idList.add(account.get("Id"));
+			}
+		}
+		flow.process(getTestEvent(idList, MessageExchangePattern.REQUEST_RESPONSE));
+	}
+
+	private Map<String, String> createAccount(String orgId, int sequence) {
+		Map<String, String> account = new HashMap<String, String>();
+
+		account.put("Name", "Name_" + sequence);
+		account.put("Id", "Id" + sequence);
+		account.put("Email", "some.email." + sequence + "@fakemail.com");
+		account.put("Description", "Some fake description");
+		account.put("MailingCity", "Denver");
+		account.put("MailingCountry", "USA");
+		account.put("MobilePhone", "123456789");
+		account.put("Department", "department_" + sequence + "_" + orgId);
+		account.put("Phone", "123456789");
+		account.put("Title", "Dr");
+
+		return account;
+	}
 }
